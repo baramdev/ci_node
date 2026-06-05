0d38c807f4eb0eea95360cd0036a7b7f44514beb
Jorbi Fernandez <jorbi.fernandez0411@gmail.com>
2026-06-05 18:08:21 -0400

    feat(add and modify) package-lock.json and modify index.test.js

--- index.test.js
+++ index.test.js
@@ -5,7 +5,3 @@ const { sumar } = require('./index');
 test('Debe sumar dos numeros correctamente', () => {
     assert.strictEqual(sumar(2, 3), 5);
 })
-
-test('Debe sumar numeros negativos correctamente', () => {
-    assert.strictEqual(sumar(2, 2), 5);
-})
